17b69e07113469e63031e2b79262d8d560853ddc
Yamashita <deminend@gmail.com>
2024-03-12 17:33:57 +0900

    initial

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # CreateAWSOrgAccount
-Organizationsでメンバーアカウントをまとめて作成して指定したOUに移動するスクリプト
+Organizationsでメンバーアカウントをまとめて作成して指定したOUに移動するスクリプトです。
+AWS CloudShellへcreate_account.pyをダウンロードして実行します。
+
+mail_prefix、mail_domain、source_parent_id、destination_parent_id、start、stopを環境にあわせて書き換えます。
+ルートユーザーのメールアドレスアカウントはプレフィックスと数字です。
+
+* mail_prefix
+ルートユーザーのメールアカウントとアカウント名のプレフィックスです。
+
+* mail_domain
+ルートユーザーのメールドメインです。
+
+* source_parent_id
+Organizationsのr-xxxxのようなルートIDです。
+
+* destination_parent_id
+OUのou-xxxxxxのようなOUのIDです。
+
+* start, stop
+ルートユーザーのメールアカウントの数字のはじまりと終わりです。
+1と50を指定すればアカウントが50作成されます。
+AWSアカウントの制限値を超えることはできませんので、クォーターを確認して実行してください。
+
+```
+python3 create_account.py
+```
---
+++ create_account.py
@@ -0,0 +1,89 @@
+import json
+import boto3
+
+from botocore.exceptions import WaiterError
+from botocore.waiter import WaiterModel
+from botocore.waiter import create_waiter_with_client
+
+mail_prefix='workshop'
+mail_domain='example.com'
+source_parent_id='r-xxxxx'
+destination_parent_id='ou-xxxxxxx'
+start=1
+stop=50
+
+delay = 2
+max_attempts = 30
+waiter_name = 'AccountCreated'
+waiter_config = {
+    'version': 2,
+    'waiters': {
+        'AccountCreated': {
+            'operation': 'DescribeCreateAccountStatus',
+            'delay': delay,
+            'maxAttempts': max_attempts,
+            'acceptors':[
+                {
+                    "matcher": "path",
+                    "expected": "IN_PROGRESS",
+                    "argument": "CreateAccountStatus.State",
+                    "state": "retry"
+                },
+                {
+                    "matcher": "path",
+                    "expected": "SUCCEEDED",
+                    "argument": "CreateAccountStatus.State",
+                    "state": "success"
+                },
+                {
+                    "matcher": "path",
+                    "expected": "FAILED",
+                    "argument": "CreateAccountStatus.State",
+                    "state": "failure"
+                }
+            ],
+        },
+    },
+}
+
+waiter_model = WaiterModel(waiter_config)
+org = boto3.client('organizations')
+
+for i in range(start, stop+1):
+    str_i = str(i)
+    response = org.create_account(
+        Email='{mail_prefix}{i}@{mail_domain}'.format(
+            mail_prefix=mail_prefix,
+            i=str_i,
+            mail_domain=mail_domain
+        ),
+        AccountName='{mail_prefix}{i}'.format(
+            mail_prefix=mail_prefix,
+            i=str_i
+        ),
+        RoleName='OrganizationAccountAccessRole'
+    )
+    print(response)
+    create_account_request_id = response.get('CreateAccountStatus').get('Id')
+
+    custom_waiter = create_waiter_with_client(
+        waiter_name=waiter_name,
+        waiter_model=waiter_model,
+        client=org
+    )
+    custom_waiter.wait(
+        CreateAccountRequestId=create_account_request_id
+    )
+
+    response = org.describe_create_account_status(
+        CreateAccountRequestId=create_account_request_id
+    )
+
+    account_id = response.get('CreateAccountStatus').get('AccountId')
+    print(account_id)
+    response = org.move_account(
+        AccountId=account_id,
+        SourceParentId=source_parent_id,
+        DestinationParentId=destination_parent_id
+    )
+    print(response)
